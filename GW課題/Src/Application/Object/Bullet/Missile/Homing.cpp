9efd160cba3d59a9be0e870cf264c1b893d9970e
motoki-0416 <kd1319689@st.kobe.denshi.as.jp>
2023-05-01 09:56:06 +0900

    03

--- GW課題/Src/Application/Object/Bullet/Missile/Homing.cpp
+++ GW課題/Src/Application/Object/Bullet/Missile/Homing.cpp
@@ -1,1 +1,45 @@
-#include "Missile.h"
+#include "Homing.h"
+
+void C_Homing::Init()
+{
+	m_deg = 0;
+	homingDelay = 40;
+}
+
+void C_Homing::Update()
+{
+
+	if (m_bAlive)m_bAlive = IsScreenOut();
+	
+	if (homingDelay-- < 0)
+	{
+
+		m_data.m_move = { cos(DirectX::XMConvertToRadians(m_deg))*-10.0f,sin(DirectX::XMConvertToRadians(m_deg))*-10.0f,0 };
+
+	}
+
+	m_data.m_pos += m_data.m_move;
+	m_scaleMat = Math::Matrix::CreateScale(m_size);
+	m_data.m_mat = m_scaleMat * Math::Matrix::CreateTranslation(m_data.m_pos);
+
+}
+
+
+void C_Homing::SetPlayerPos(Math::Vector3 a_pos)
+{
+	Math::Vector3 dist;
+
+
+	dist = m_data.m_pos - a_pos;
+
+
+	m_deg = DirectX::XMConvertToDegrees(atan2(dist.y, dist.x));
+
+
+	if (m_deg < 0)
+	{
+
+		m_deg += 360;
+
+	}
+}
